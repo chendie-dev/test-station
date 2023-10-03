3e3ed7710e7ffb02f20f78d1860ac7f02ef8d55e
ddgo <1812805089@qq.com>
2023-10-04 00:17:02 +0800

    考试详细记录

--- src/main/java/com/chendie/teststation/controller/ExamRecordDetailController.java
+++ src/main/java/com/chendie/teststation/controller/ExamRecordDetailController.java
@@ -1,18 +1,26 @@
 package com.chendie.teststation.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.chendie.teststation.entity.ExamRecord;
 import com.chendie.teststation.entity.ExamRecordDetail;
+import com.chendie.teststation.entity.Question;
+import com.chendie.teststation.entity.Tag;
 import com.chendie.teststation.model.ResultView;
 import com.chendie.teststation.service.IExamRecordDetailService;
+import com.chendie.teststation.service.IExamRecordService;
+import com.chendie.teststation.service.IQuestionService;
+import com.chendie.teststation.service.ITagService;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -27,6 +35,12 @@ import java.util.Objects;
 public class ExamRecordDetailController {
     @Resource
     private IExamRecordDetailService examRecordDetailService;
+    @Resource
+    private IExamRecordService examRecordService;
+    @Resource
+    private IQuestionService questionService;
+    @Resource
+    private ITagService tagService;
 
     @PostMapping("/getExamRecordDetail")
     public ResultView<ExamRecordDetail> getExamRecordDetail(
@@ -40,5 +54,33 @@ public class ExamRecordDetailController {
             return ResultView.success(examRecordDetails.get(0));
         }
         return ResultView.success();
+    }
+
+    @PostMapping("/getErrorTag")
+    public ResultView<List<Tag>> getErrorTag(
+            @RequestParam("userId") Long userId
+    ) {
+        // 获取所有的错误考试回答的原问题id列表
+        List<ExamRecord> examRecords = examRecordService
+                .list(new LambdaQueryWrapper<ExamRecord>()
+                        .eq(ExamRecord::getUserId, userId));
+        List<Long> recordIds = examRecords.stream()
+                .map(ExamRecord::getRecordId)
+                .collect(Collectors.toList());
+        List<Long> questionIds = examRecordDetailService
+                .list(new LambdaQueryWrapper<ExamRecordDetail>()
+                        .in(!CollectionUtils.isEmpty(recordIds), ExamRecordDetail::getRecordId, recordIds))
+                .stream()
+                .filter(examRecordDetail -> examRecordDetail.getScore() == 0)
+                .map(ExamRecordDetail::getQuestionId)
+                .distinct()
+                .collect(Collectors.toList());
+        // 获取对应的标签
+        List<Long> tagIds = questionService.listByIds(questionIds).stream()
+                .map(Question::getTagId)
+                .distinct()
+                .collect(Collectors.toList());
+        List<Tag> tags = tagService.listByIds(tagIds);
+        return ResultView.success(tags);
     }
 }
