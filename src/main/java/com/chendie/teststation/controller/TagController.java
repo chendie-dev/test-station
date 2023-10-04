aaac30bbb549c828ccef90e0985cbadb66de2bd6
ddgo <1812805089@qq.com>
2023-10-04 19:00:31 +0800

    标签创建专项训练

--- src/main/java/com/chendie/teststation/controller/TagController.java
+++ src/main/java/com/chendie/teststation/controller/TagController.java
@@ -3,11 +3,13 @@ package com.chendie.teststation.controller;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.chendie.teststation.convert.TagConvert;
+import com.chendie.teststation.entity.Paper;
 import com.chendie.teststation.entity.Tag;
 import com.chendie.teststation.model.IdView;
 import com.chendie.teststation.model.PageQry;
 import com.chendie.teststation.model.PageResult;
 import com.chendie.teststation.model.ResultView;
+import com.chendie.teststation.service.IPaperService;
 import com.chendie.teststation.service.ITagService;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,15 +36,29 @@ public class TagController {
 
     @Resource
     private ITagService tagService;
+    @Resource
+    private IPaperService paperService;
 
     @PostMapping("/addOrUpdateTag")
     public ResultView<IdView> addOrUpdateTag(
             @RequestBody Tag tag
     ) {
+        // 新增标签创建对应的试卷
+        Paper paper = null;
+        if (Objects.isNull(tag.getTagId())) {
+            paper = new Paper();
+            paper.setTime(Long.MAX_VALUE);
+            paper.setTitle(tag.getTagName() + "专项训练");
+            paper.setUserId(0L);
+        }
         boolean ok = tagService.saveOrUpdate(tag);
         IdView idView = IdView.builder().build();
         if (ok) {
             idView.setId(tag.getTagId());
+        }
+        if (Objects.nonNull(paper)) {
+            paper.setTagId(tag.getTagId());
+            paperService.save(paper);
         }
         return ResultView.success(idView);
     }
