9b0f7e3a4f64ded7008e111a9bbf2894f98753e7
ddgo <1812805089@qq.com>
2023-10-14 21:11:24 +0800

    rolename 查询

--- src/main/java/com/chendie/teststation/controller/UserController.java
+++ src/main/java/com/chendie/teststation/controller/UserController.java
@@ -79,7 +79,7 @@ public class UserController {
         // 条件
         queryWrapper
                 .eq(Objects.nonNull(queryParam.getUserId()), User::getUserId, queryParam.getUserId())
-                .eq(User::getRoleName, "student")
+                .eq(Objects.nonNull(queryParam.getRoleName()), User::getRoleName, queryParam.getRoleName())
                 .like(Objects.nonNull(queryParam.getRealName()), User::getRealName, queryParam.getRealName());
         // 排序规则
         LinkedHashMap<String, Boolean> orderByFields = userPageQry.getOrderByFields();
